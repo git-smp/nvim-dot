e70579ef90c188403850362033d5392b66054fbf
Santiago Munevar <santiago@mykargo.com>
2023-11-29 21:44:29 -0800

    Commiting current state

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,16 +14,100 @@ require("lazy").setup({
       colorscheme = "gruvbox",
     } },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- Install other plugins
     { "ellisonleao/gruvbox.nvim" },
+    -- SQL stuff
     { "tpope/vim-dadbod" },
     { "kristijanhusak/vim-dadbod-ui" },
-
+    -- Commenting out as potential nvim-cmp conflict
+    -- { "kristijanhusak/vim-dadbod-completion" },
+    -- Install neorg
+    -- {
+    --   "nvim-neorg/neorg",
+    --   run = ":Neorg sync-parsers",
+    -- },
+    {
+      "pwntester/octo.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("octo").setup({ enable_builtin = true })
+        vim.cmd([[hi OctoEditable guibg=none]])
+      end,
+      keys = {
+        { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
+      },
+    },
+    -- {
+    --   "epwalsh/obsidian.nvim",
+    --   lazy = false,
+    --   event = {
+    --     "BufReadPre "
+    --       .. vim.fn.expand("~")
+    --       .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/Santis Vault/**.md",
+    --     "BufNewFile "
+    --       .. vim.fn.expand("~")
+    --       .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/Santis Vault/**.md",
+    --   },
+    --   dependencies = {
+    --     -- Required.
+    --     "nvim-lua/plenary.nvim",
+    --   },
+    --   opts = {
+    --     dir = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Santis Vault/",
+    --     workspaces = {
+    --       {
+    --         name = "personal",
+    --         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Santis Vault/",
+    --       },
+    --     },
+    --     daily_notes = {
+    --       -- Optional, if you keep daily notes in a separate directory.
+    --       folder = "Notes/Daily",
+    --       -- Optional, if you want to change the date format for daily notes.
+    --       date_format = "%Y-%m-%d",
+    --       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    --       template = "Daily Notes Template.md",
+    --     },
+    --     templates = {
+    --       subdir = "Templates",
+    --       date_format = "%Y-%m-%d-%a",
+    --       time_format = "%H:%M",
+    --     },
+    --     -- Optional, set to true if you use the Obsidian Advanced URI plugin.
+    --     -- https://github.com/Vinzent03/obsidian-advanced-uri
+    --     use_advanced_uri = true,
+    --   },
+    -- },
+    {
+      "jackMort/ChatGPT.nvim",
+      event = "VeryLazy",
+      config = function()
+        require("chatgpt").setup({
+          openai_params = {
+            model = "gpt-4-1106-preview",
+            frequency_penalty = 0,
+            presence_penalty = 0,
+            max_tokens = 300,
+            temperature = 0,
+            top_p = 1,
+            n = 1,
+          },
+        })
+      end,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+      },
+    },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
--- lua/plugins/vim-dadbod.lua
+++ lua/plugins/vim-dadbod.lua
@@ -1,6 +1,32 @@
-vim.keymap.set("n", "<leader>sqo", ":DBUIToggle<CR>")
-vim.keymap.set("n", "<leader>sqf", ":DBUIFindBuffer<CR>")
-vim.keymap.set("n", "<leader>sqr", ":DBUIRenameBuffer<CR>")
-vim.keymap.set("n", "<leader>sql", ":DBUILastQueryInfo<CR>")
+local wk = require("which-key")
+wk.register({
+  sq = {
+    name = "Dadbod",
+    o = { "<cmd>DBUIToggle<CR>", "Toggle" },
+    f = { "<cmd>DBUIFindBuffer<CR>", "Edit with instruction", mode = { "n", "v" } },
+    r = { "<cmd>DBUIRenameBuffer<CR>", "Grammar Correction", mode = { "n", "v" } },
+    l = { "<cmd>DBUILastQueryInfo<CR>", "Translate", mode = { "n", "v" } },
+  },
+}, { prefix = "<leader>" })
+
+vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = {
+--     "sql",
+--   },
+--   command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+-- })
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = {
+--     "sql",
+--     "mysql",
+--     "plsql",
+--   },
+--   callback = function()
+--     vim.schedule(db_completion)
+--   end,
+-- })
 
 return {}
